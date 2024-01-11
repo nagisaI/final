326068047d3dafc08f6d3008c686f3fa19ff9bb2
nagisaI <2201353@s.asojuku.ac.jp>
2024-01-11 10:39:04 +0900

    Create db_connect.php

---
+++ src/db_connect.php
@@ -0,0 +1,7 @@
+<?php
+    const SERVER = 'mysql220.phy.lolipop.lan';
+    const DBNAME = 'LAA1518108-final';
+    const USER = 'LAA1518108';
+    const PASS = 'Pass1209';
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
